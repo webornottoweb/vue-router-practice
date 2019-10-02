43dc07fbb17205c595a4a3de65cb74dd4ecab6ec
Andrey Palamarchuk <a.palamarchuk@corp.101xp.com>
2019-10-02 23:43:46 +0300

    Added button for programmatically push router state

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         Profile page
+        <button class="btn btn-info" @click="showProjects">Show projects</button>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        showProjects() {
+            this.$router.push('/projects');
+        }
+    }
 }
 </script>
 <style scoped>
