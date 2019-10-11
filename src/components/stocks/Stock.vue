2fa4461e74c6a8ade318e82fe8ae6128eac022a1
Andrey Palamarchuk <aannppaall@gmail.com>
2019-10-11 17:11:48 +0300

    Fixed mutations vuex store prop name

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,7 +3,7 @@
         <img class="card-img-top" :src="img" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">{{ stock.name }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Price: {{ stock.price }}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">Price: {{ stock.currency }} {{ stock.price }}</h6>
             <p class="card-text">{{ stock.description }}</p>
             <div class="d-flex flex-row justify-content-between">
                 <input type="number" class="form-control flex-fill" style="width: 150px;" placeholder="Amount" v-model.number="quantity" />
--- src/store/store.js
+++ src/store/store.js
@@ -10,7 +10,7 @@ export default new Vuex.Store({
         stocks
     },
     getters: {},
-    mutators: {},
+    mutations: {},
     actions: {},
     modules: {}
 });
