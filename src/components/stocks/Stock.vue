5eb0b7dd51783cea9a9ad7c9f39df66f3a664201
Andrey Palamarchuk <a.palamarchuk@corp.101xp.com>
2019-10-11 20:12:56 +0300

    Added purchasing process overlay (for stock card)

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="card" style="width: 18rem;">
+        <div class="card-overlay" v-if="buying">
+            <div class="spinner-border text-light" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
         <img class="card-img-top" :src="img" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">{{ stock.name }}</h5>
@@ -7,10 +12,7 @@
             <p class="card-text">{{ stock.description }}</p>
             <div class="d-flex flex-row justify-content-between">
                 <input type="number" class="form-control flex-fill" style="width: 150px;" placeholder="Amount" v-model.number="quantity" />
-                <button class="btn btn-primary buy-btn" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">
-                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" v-if="buying"></span>
-                    Buy
-                </button>
+                <button class="btn btn-primary buy-btn" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">Buy</button>
             </div>
         </div>
     </div>
@@ -63,5 +65,16 @@ export default {
     }
     .buy-btn {
         margin-left: 10px;
+    }
+    .card-overlay {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 1;
+        background-color: lightblue;
+        opacity: .9;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 </style>
